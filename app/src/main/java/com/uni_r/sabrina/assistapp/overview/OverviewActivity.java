a8cba5e3bd1662b4a39f4f504b2255cf1a9e123a
Sabse <sabseloliert@gmail.com>
2015-11-03 05:31:42 +0100

    Usability in Overview mit ScrollTo und Farbwechsel verbessert Text ausgebessert bei Diabetes

--- app/src/main/java/com/uni_r/sabrina/assistapp/overview/OverviewActivity.java
+++ app/src/main/java/com/uni_r/sabrina/assistapp/overview/OverviewActivity.java
@@ -1,12 +1,15 @@
 package com.uni_r.sabrina.assistapp.overview;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.uni_r.sabrina.assistapp.R;
@@ -15,6 +18,8 @@ import com.uni_r.sabrina.assistapp.main.EmergencyCallActivity;
 import com.uni_r.sabrina.assistapp.responseless.ResponselessActivity;
 
 import org.apache.http.conn.ConnectionReleaseTrigger;
+
+
 
 /**
  * Created by Sabse on 24.05.2015.
@@ -71,57 +76,157 @@ public class OverviewActivity extends EmergencyCallActivity{
         startActivity(intent);
     }
 
+    private final void focusOnView(final View v, final View v2){
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                v.scrollTo(0, v2.getTop());
+            }
+        });
+    }
+
     public void expandTitle(View v){
+
         switch (v.getTag().toString()){
 
 
             case "injury2": contentLayout = (RelativeLayout) findViewById(R.id.injury_title_2_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.injury_scroll), findViewById(R.id.injury_title_2));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "injury3": contentLayout = (RelativeLayout) findViewById(R.id.injury_title_3_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.injury_scroll), findViewById(R.id.injury_title_3));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "injury4": contentLayout = (RelativeLayout) findViewById(R.id.injury_title_4_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.injury_scroll), findViewById(R.id.injury_title_4));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "injury5": contentLayout = (RelativeLayout) findViewById(R.id.injury_title_5_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.injury_scroll), findViewById(R.id.injury_title_5));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "injury6": contentLayout = (RelativeLayout) findViewById(R.id.injury_title_6_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.injury_scroll), findViewById(R.id.injury_title_6));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "injury7": contentLayout = (RelativeLayout) findViewById(R.id.injury_title_7_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.injury_scroll), findViewById(R.id.injury_title_7));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
 
             case "head1": contentLayout = (RelativeLayout) findViewById(R.id.head_title_1_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.head_scroll), findViewById(R.id.head_title_1));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "head2": contentLayout = (RelativeLayout) findViewById(R.id.head_title_2_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.head_scroll), findViewById(R.id.head_title_2));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "head3": contentLayout = (RelativeLayout) findViewById(R.id.head_title_3_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.head_scroll), findViewById(R.id.head_title_3));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
 
             case "chest1": contentLayout = (RelativeLayout) findViewById(R.id.chest_title_1_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.chest_scroll), findViewById(R.id.chest_title_1));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "chest2": contentLayout = (RelativeLayout) findViewById(R.id.chest_title_2_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.chest_scroll), findViewById(R.id.chest_title_2));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "chest3": contentLayout = (RelativeLayout) findViewById(R.id.chest_title_3_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.chest_scroll), findViewById(R.id.chest_title_3));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "chest4": contentLayout = (RelativeLayout) findViewById(R.id.chest_title_4_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.chest_scroll), findViewById(R.id.chest_title_4));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
 
             case "stomach1": contentLayout = (RelativeLayout) findViewById(R.id.stomach_title_1_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.stomach_scroll), findViewById(R.id.stomach_title_1));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
             case "stomach2": contentLayout = (RelativeLayout) findViewById(R.id.stomach_title_2_content);
                 contentLayout.setVisibility(contentLayout.isShown() ? View.GONE : View.VISIBLE);
+                if(contentLayout.isShown()){
+                    v.setBackgroundColor(getResources().getColor(R.color.alert_red));
+                    focusOnView(findViewById(R.id.stomach_scroll), findViewById(R.id.stomach_title_2));
+                }else{
+                    v.setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
                 break;
 
             default: break;
